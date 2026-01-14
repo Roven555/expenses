6f0a421b9d15cb5b85acb2fd81f552306579a69f
Student <student@Administrators-Mac-mini.local>
2026-01-14 16:34:05 +0200

    sss

--- expenses/src/App.jsx
+++ expenses/src/App.jsx
@@ -1,7 +1,26 @@
-import Header from "/Header.jsx";
+import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
 
-function App() {
-  return <Header />;
-}
+const App = () => {
+  const expenses = [
+    {
+      date: new Date(2024, 10, 12),
+      title: "New book",
+      price: 30.99,
+    },
+    {
+      date: new Date(2024, 10, 12),
+      title: "New jeans",
+      price: 99.99,
+    },
+  ];
+
+  return (
+    <div className="App">
+      <ExpenseItem data={expenses[0]} />
+      <ExpenseItem data={expenses[1]} />
+    </div>
+  );
+};
 
 export default App;
