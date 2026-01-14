9eade5be07b94154dcd6f9c979d26ed243a8981c
Student <student@Administrators-Mac-mini.local>
2026-01-14 16:44:10 +0200

    Ülesanne 1

--- expenses/src/components/ExpenseItem.jsx
+++ expenses/src/components/ExpenseItem.jsx
@@ -1,15 +1,13 @@
-import "./ExpenseItem.css";
+import ExpenseDate from './ExpenseDate';
+import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-  console.log(props);
-  console.log(props.data);
-
   return (
-    <div className="expense-item">
-      <div>{props.data.date.toString()}</div>
-      <div className="expense-item__description">
+    <div className='expense-item'>
+      <ExpenseDate date={props.data.date} />
+      <div className='expense-item__description'>
         <h2>{props.data.title}</h2>
-        <div className="expense-item__price">{props.data.price}</div>
+        <div className='expense-item__price'>{props.data.price}</div>
       </div>
     </div>
   );
