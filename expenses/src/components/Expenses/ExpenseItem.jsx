3ae708869a4113c6762bc2d19492a952eb48ab9e
Roven Piho <roven.piho@voco.ee>
2026-04-07 15:52:35 +0300

    Õige 11. Vormitöötlus Reactis.

--- expenses/src/components/Expenses/ExpenseItem.jsx
+++ expenses/src/components/Expenses/ExpenseItem.jsx
@@ -4,13 +4,6 @@ import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-  //const [title, setTitle] = useState(props.title);//
-
-  const clickHandler = () => {
-    console.log("Clicked!");
-    // setTitle("Updated!") //
-  };
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -18,7 +11,6 @@ function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Click me</button>
     </Card>
   );
 }
--- expenses/src/components/Expenses/Expenses.jsx
+++ expenses/src/components/Expenses/Expenses.jsx
@@ -6,14 +6,14 @@ function Expenses(props) {
   return (
     <Card className="expenses">
       <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
+        title={props.expenses[0].title}
+        amount={props.expenses[0].amount}
+        date={props.expenses[0].date}
       />
       <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
+        title={props.expenses[1].title}
+        amount={props.expenses[1].amount}
+        date={props.expenses[1].date}
       />
     </Card>
   );
